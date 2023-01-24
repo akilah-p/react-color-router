a8663eb2e258248c8bb2b008a9d2ae989e4e8232
Akilah Powell <atpjun@gmail.com>
2023-01-24 11:18:45 -0800

    added components and all folders within it. added notFound js added path to appjs

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-
 import { Route, Switch } from 'react-router-dom';
 import './App.css';
+import Header from './components/Header/Header.js';
+import Main from './components/Main/Main';
 
 function App() {
   return (
     <div className="App">
+      <Header />     
       <Switch>
-        <Route></Route>
+        <Route path="/rbg/:red/:green/:blue">
+          <Main />
+        </Route>
       </Switch>
-      
     </div>
   );
 }
