ad4825e25e67999a7ddbaee51f246d54c0b7a45e
Akilah Powell <atpjun@gmail.com>
2023-01-24 15:07:29 -0800

    everything displaying working on styling, still need to create home and not found page

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,10 +4,10 @@ import { NavLink } from 'react-router-dom';
 
 export default function Header() {
   return (
-    <div className="header">
-      <NavLink to="/rbg/255/0/0">Red</NavLink>
-      <NavLink to="/rbg/0/255/0">Green</NavLink>
-      <NavLink to="/rbg/0/0/255">Blue</NavLink>
-    </div>
+    <ul className="header">
+     <li className="red"> <NavLink to="/rbg/255/0/0">Red</NavLink></li>
+     <li className="green"> <NavLink to="/rbg/0/255/0">Green</NavLink></li>
+      <li className="blue"><NavLink to="/rbg/0/0/255">Blue</NavLink></li>
+    </ul>
   );
 }
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import './Main.css';
 
 export default function Main() {
   const { red, green, blue } = useParams();
   return (
     <>
-      <div className="main" style={{ height: '100vh', width:'100vw', background: `rgb(${red}, ${green}, ${blue})` }}>
-        ({red}, {green}, {blue})
+      <div className="main" style={{ background: `rgb(${red}, ${green}, ${blue})` }}>
+      RGB({red}, {green}, {blue})
       </div>
     </>
   );
